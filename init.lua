4bace0bfbdfd1c802f839d643be13021b4f7817d
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-01-27 20:40:10 +0900

    行の入れ替えのショートカット追加

--- init.lua
+++ init.lua
@@ -14,3 +14,11 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
+-- 行を上下に移動(ノーマルモード)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+
+-- Visualモードでの移動
+vim.keymap.set("x", "<A-k>", ":m <-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("x", "<A-j>", ":m >+1<CR>gv=gv", { noremap = true, silent = true })
+
